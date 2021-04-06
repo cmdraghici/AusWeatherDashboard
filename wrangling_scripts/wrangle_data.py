71a6c777f378893a526c1376de106b1f775db694
Cezar Mihai Draghici <cmdraghici.92@gmail.com>
2021-04-06 22:49:30 +0200

    Add data wragling for the weather temperature

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -4,6 +4,20 @@ import plotly.graph_objs as go
 # Use this file to read in your data and prepare the plotly visualizations. The path to the data files are in
 # `data/file_name.csv`
 
+def generateWeatherTable():
+    df = pd.read_csv('data/weatherAUS.csv')
+    dfTemperature = df[['MinTemp', 'MaxTemp']]
+    dfDates = df["Date"].str.split("-", n = 2, expand = True)
+    dfWeather = pd.concat([dfDates, dfTemperature], axis=1).rename(columns={0: 'Year', 1:'Month', 2:'Day'})
+    dfWeather = dfWeather.dropna()
+    return dfWeather
+	
+def AusWeatherByMonths(df) :
+    return df.groupby(['Month']).mean().rename(index={'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August','09': 'September', '10': 'October', '11': 'November', '12': 'December'})
+	
+def AusWeatherByYear(df):
+    return df.groupby(['Year']).mean()
+
 def return_figures():
     """Creates four plotly visualizations
 
@@ -17,34 +31,39 @@ def return_figures():
 
     # first chart plots arable land from 1990 to 2015 in top 10 economies 
     # as a line chart
-    
-    graph_one = []    
+
+    df = generateWeatherTable()
+    dfWatherMonths = AusWeatherByMonths(df)
+    dfWatherYear = AusWeatherByYear(df)
+	
+    graph_one = []
     graph_one.append(
       go.Scatter(
-      x = [0, 1, 2, 3, 4, 5],
-      y = [0, 2, 4, 6, 8, 10],
+      x = list(dfWatherMonths.index),
+      y = list(dfWatherMonths["MinTemp"]),
       mode = 'lines'
       )
     )
 
-    layout_one = dict(title = 'Chart One',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    layout_one = dict(title = 'Minimum Temperature per Month',
+                xaxis = dict(title = 'Months'),
+                yaxis = dict(title = 'Temperature'),
                 )
 
 # second chart plots ararble land for 2015 as a bar chart    
     graph_two = []
 
     graph_two.append(
-      go.Bar(
-      x = ['a', 'b', 'c', 'd', 'e'],
-      y = [12, 9, 7, 5, 1],
+      go.Scatter(
+      x = list(dfWatherMonths.index),
+      y = list(dfWatherMonths["MaxTemp"]),
+	  mode = 'lines'
       )
     )
 
-    layout_two = dict(title = 'Chart Two',
-                xaxis = dict(title = 'x-axis label',),
-                yaxis = dict(title = 'y-axis label'),
+    layout_two = dict(title = 'Maximum Temperature per Month',
+                xaxis = dict(title = 'Months',),
+                yaxis = dict(title = 'Temperature'),
                 )
 
 
@@ -52,15 +71,15 @@ def return_figures():
     graph_three = []
     graph_three.append(
       go.Scatter(
-      x = [5, 4, 3, 2, 1, 0],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
+      x = list(dfWatherYear.index),
+      y = list(dfWatherYear["MinTemp"]),
+	  mode = 'lines'
       )
     )
 
-    layout_three = dict(title = 'Chart Three',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label')
+    layout_three = dict(title = 'Minimum Temperature per Year',
+                xaxis = dict(title = 'Years'),
+                yaxis = dict(title = 'Temperature')
                        )
     
 # fourth chart shows rural population vs arable land
@@ -68,15 +87,15 @@ def return_figures():
     
     graph_four.append(
       go.Scatter(
-      x = [20, 40, 60, 80],
-      y = [10, 20, 30, 40],
-      mode = 'markers'
+      x = list(dfWatherYear.index),
+      y = list(dfWatherYear["MaxTemp"]),
+	  mode = 'lines'
       )
     )
 
-    layout_four = dict(title = 'Chart Four',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    layout_four = dict(title = 'Maximum Temperature per Year',
+                xaxis = dict(title = 'Years'),
+                yaxis = dict(title = 'Temperature'),
                 )
     
     # append all charts to the figures list
